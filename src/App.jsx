85bd540bb3bf32c0f5d6cfea5ed9b816aafc7b2d
smith <aptl741@gmail.com>
2023-08-13 22:05:31 +0900

    레이아웃

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
+import { Routes, Route, Link} from "react-router-dom";
 
-import './App.css'
+import Head from "./Head/head";
+import Home from "./Home/home";
+import About from "./List/list";
+import Dashboard from "./Main/main";
+import NoMatch   from "./VocaLearn/vocaLearn";
 
-function App() {
-  
-
+export default function App() {
   return (
-    <>
-      <div>
-        hi
-      </div>
-    </>
-  )
+    <div>
+      <header>
+        <nav className="menu">
+          <a href="/" data-menu="two">
+            Home
+          </a>
+
+          <a href="/" data-menu="two">
+            Create VocaList
+          </a>
+
+          <a href="/" data-menu="three">
+            Daily Check
+          </a>
+
+        </nav>
+
+        <Link to="/about">about</Link>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      {/* <Routes>
+          <Route path="/" element={<Head />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes> */}
+    </div>
+  );
 }
 
-export default App
+
+
+
+
+
---
+++ src/List/list.jsx
@@ -0,0 +1,21 @@
+// const List = () => {
+//   return (
+//     <>
+//     <div>
+//       hi
+//     </div>
+//     </>
+//   )
+// }
+
+// export default List
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+export default About;
