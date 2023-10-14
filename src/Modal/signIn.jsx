0752348a4a25469fc786e123e0ca24f11eb6ea67
smith <aptl741@gmail.com>
2023-10-14 12:34:41 +0900

    로그인 페이지 구성

--- src/Modal/signIn.jsx
+++ src/Modal/signIn.jsx
@@ -1,33 +1,100 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
+
 import "./signIn.css";
+import axios from "axios";
 
 const Modal = ({ handleClose }) => {
   const [isRegister, setIsRegister] = useState(false);
+  const [Id, setId] = useState("");
+  const [Password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleSwitch = () => {
     setIsRegister(!isRegister);
+    setId("")
+    setPassword("");
+    setConfirmPassword("");
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isRegister && Password !== confirmPassword) {
+      alert("비밀번호 불일치!");
+      return;
+    }
+
+    const requestData = {
+      id: Id,
+      password: Password
+    }
+    const api = import.meta.env.VITE_API_URL;
+   
+    axios
+      .post(`${api}/user/sign-in`, requestData)
+      .then((response) => {
+        console.log(requestData);
+        console.log(response.data);
+        handleClose();
+      })
+      .catch((error) => {
+        console.log(requestData);
+        console.log(error);
+      });
+  }
+    
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <form onSubmit=" ">
+        <form onSubmit={handleSubmit}>
           <h1>VocaLearner</h1>
           {!isRegister ? (
             <>
-              <input type="id" placeholder="id" />
-              <input type="password" placeholder="password" />
-              <button onClick={handleClose}>로그인</button>
+              <input
+                type="id"
+                name="id"
+                value={Id}
+                onChange={(e) => setId(e.target.value)}
+                placeholder="id"
+              />
+              <input
+                type="password"
+                name="password"
+                value={Password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="password"
+              />
+              {/* <button onClick={handleClose}>로그인</button> */}
+              <button>로그인</button>
             </>
           ) : (
             <>
-              <input type="id" placeholder="id" />
-              <input type="password" placeholder="password" />
-              <input type="passwordCheck" placeholder="passwordCheck" />
-              <button onClick={handleClose}>회원가입</button>
+              <input
+                type="id"
+                name="id"
+                value={Id}
+                onChange={(e) => setId(e.target.value)}
+                placeholder="아이디"
+              />
+              <input
+                type="password"
+                name="password"
+                value={Password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="비밀번호"
+              />
+              <input
+                type="password"
+                name="confirmPassword"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                placeholder="비밀번호 확인"
+              />
+              {/* <button onClick={handleClose}>회원가입</button> */}
+              <button>회원가입</button>
             </>
           )}
           {!isRegister ? (
