fe171c7e085db67e4f8ffc4682b75bb51a1cfae9
smith <aptl741@gmail.com>
2023-10-18 01:49:46 +0900

    단어장 생성

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import Home from "./Home/home";
-import About from "./List/list";
 import Modal from "./Modal/signIn";
-
+import ListCreate from './Modal/listCreate'
 
 import "./app.css"
 
 export default function App() {
- const [showModal, setShowModal] = useState(false);
+ const [signInModal, setSignInModal] = useState(false);
+ const [listCreateModal, setListCreateModal] = useState(false);
  const [userData, setUserData] = useState(null);
  const [listData, setListData] = useState([]);
  const [error, setError] = useState(false);
 
  useEffect(() => {
-   setShowModal(true);
+   setSignInModal(true);
  }, []);
 
 useEffect(() => {
@@ -46,34 +46,72 @@ useEffect(() => {
 }, [userData]);
 
 
- const handleClose = () => {
-   setShowModal(false);
+ const handleSignInClose = () => {
+   setSignInModal(false);
  };
+
+ const listCreateClose = () => {
+  setListCreateModal(false);
+ }
 
  const handleData = (Data) => {
    setUserData(Data)
 }
+
+const refreshLists = () => {
+  if (userData) {
+    const userInfo = userData.data.data;
+    const token = userInfo.accessToken;
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const api = import.meta.env.VITE_API_URL;
+
+    axios
+      .get(`${api}/list/user_key/${userInfo.key}`, config)
+      .then((response) => {
+        console.log(response);
+        setListData(response.data.data);
+        setError(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(true);
+      });
+  }
+};
  
   return (
     <div>
-      {showModal && <Modal handleClose={handleClose} handleData={handleData} />}
+      {signInModal && (
+        <Modal handleClose={handleSignInClose} handleData={handleData} />
+      )}
+      {listCreateModal && (
+        <ListCreate
+          handleClose={listCreateClose}
+          userData={userData}
+          refreshLists={refreshLists}
+        />
+      )}
 
       <header>
         <Link to="/" className="header-list">
           Home
         </Link>
-        <Link to="/" className="header-list">
+        <Link onClick={() => setListCreateModal(true)} className="header-list">
           Create VocaList
         </Link>
-        <Link to="/about" className="header-list">
+        <Link to="/" className="header-list">
           Daily Check
         </Link>
       </header>
 
       <Routes>
         <Route path="/" element={<Home listName={listData} error={error} />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
---
+++ src/Modal/listCreate.jsx
@@ -0,0 +1,60 @@
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+import axios from "axios";
+
+import "./listCreate.css";
+
+const ListCreate = ({ handleClose, userData, refreshLists }) => {
+  const [listName, setListName] = useState("");
+
+  const createListReq = () => {
+    const userInfo = userData.data.data;
+    const token = userInfo.accessToken;
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const createListData = {
+      user_key: userInfo.key,
+      name: listName,
+    };
+
+    const api = import.meta.env.VITE_API_URL;
+
+    axios
+      .post(`${api}/list`, createListData, config)
+      .then((response) => {
+        console.log(response);
+        handleClose();
+        refreshLists();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className="frame">
+      <h2>단어장 이름</h2>
+      <input
+        type="text"
+        name="name"
+        value={listName}
+        onChange={(e) => setListName(e.target.value)}
+      />
+      <button onClick={createListReq}>생성</button>
+      <button onClick={handleClose}>취소</button>
+    </div>
+  );
+};
+
+ListCreate.propTypes = {
+  handleClose: PropTypes.func.isRequired,
+  userData: PropTypes.object,
+  refreshLists: PropTypes.func.isRequired,
+};
+
+export default ListCreate
